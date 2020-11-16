525741b0b514f0cfdc7b6f6f6e9b5f8d817047aa
Tove Seger <tove.seger@outlook.com>
2020-11-16 10:35:01 +0100

    Add project files.

---
+++ Inlämning1-objektorienterad programmering/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inlämning1_objektorienterad_programmering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
